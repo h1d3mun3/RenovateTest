aa3bc5862285761d049cc7015bb3dc5c3387e408
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2022-09-22 01:05:30 +0900

    Organize code style.

--- .github/scripts/unlockPackage.sh
+++ .github/scripts/unlockPackage.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 /usr/libexec/PlistBuddy -c "Set :objects:12B0503228DB0804004950B3:requirement:kind upToNextMajorVersion" RenovateTest.xcodeproj/project.pbxproj
 /usr/libexec/PlistBuddy -c "Delete :objects:12B0503228DB0804004950B3:requirement:version" RenovateTest.xcodeproj/project.pbxproj
 /usr/libexec/PlistBuddy -c "Add :objects:12B0503228DB0804004950B3:requirement:minimumVersion string 5.1.1" RenovateTest.xcodeproj/project.pbxproj
